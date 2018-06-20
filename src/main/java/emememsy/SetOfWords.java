b2042d47a21b80b5edf53bea9353ad182c56bdb0
Monika Luba <lubamonika@gmail.com>
2018-06-20 21:16:29 +0200

    added SingleWords, SetOfWords

--- src/main/java/emememsy/SetOfWords.java
+++ src/main/java/emememsy/SetOfWords.java
@@ -1,4 +1,22 @@
 package emememsy;
 
+import java.util.Set;
+
 public class SetOfWords {
+
+    Set<SingleWord> words;
+
+    public Set<SingleWord> getWords() {
+        return words;
+    }
+
+    public void setWords(Set<SingleWord> words) {
+        this.words = words;
+    }
+
+    public void addWord(SingleWord singleWord) {
+        singleWord.setSetOfWords(this);
+        words.add(singleWord);
+    }
+
 }
--- src/main/java/emememsy/SingleWord.java
+++ src/main/java/emememsy/SingleWord.java
@@ -1,4 +1,76 @@
 package emememsy;
 
+import java.util.Objects;
+
 public class SingleWord {
+
+    private String word;
+    private String translation;
+    private int counter;
+    private SetOfWords setOfWords;
+
+    public SingleWord(String word, String translation, int counter) {
+        this.word = word;
+        this.translation = translation;
+        this.counter = counter;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public SetOfWords getSetOfWords() {
+        return setOfWords;
+    }
+
+    public void setSetOfWords(SetOfWords setOfWords) {
+        this.setOfWords = setOfWords;
+    }
+
+    @Override
+    public String toString() {
+        return "SingleWord{" +
+                "word='" + word + '\'' +
+                ", translation='" + translation + '\'' +
+                ", counter=" + counter +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SingleWord)) return false;
+        SingleWord that = (SingleWord) o;
+        return counter == that.counter &&
+                Objects.equals(word, that.word) &&
+                Objects.equals(translation, that.translation);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(word, translation, counter);
+    }
+
+
 }
