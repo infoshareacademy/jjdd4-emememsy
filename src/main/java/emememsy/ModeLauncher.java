39b5b5f6c0d5035a222eeaa1d1f89b8a848e9360
marcinpt <marcinpt@onet.pl>
2018-06-22 07:11:53 +0200

    classes modeling part-2

--- src/main/java/emememsy/ModeLauncher.java
+++ src/main/java/emememsy/ModeLauncher.java
@@ -3,8 +3,13 @@ package emememsy;
 import java.util.HashMap;
 import java.util.Map;
 
+
+//klasa tworzaca oiekt wybranego trybu - "uruchomienie" (...?)
 public class ModeLauncher {
 
     private static Map<String, Modes> allModes = new HashMap<String, Modes>();
 
+    //konstruktor tworzacy obiekt wybranego trybu
+    //public ModeLauncher( argument-okreslajacy-tryb ) {
+    }
 }
