7e41306384f17a02c5fac91927f4bd014af3ab87
Marcin Piatek <marcinpt@onet.pl>
2018-06-25 23:39:13 +0900

    ModeLauncher cont plus new main App

--- src/main/java/emememsy/App.java
+++ src/main/java/emememsy/App.java
@@ -2,18 +2,28 @@ package emememsy;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Main class application
- *
  */
 
 public class App {
-    public static void main( String[] args ) {
-        Map<String, String> modeList = new HashMap<String, String>();
+    public static void main(String[] args) {
+        ModeLauncher mL = new ModeLauncher();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Witamy w myWords.");
+            System.out.println("Masz do wyboru: " +
+                    "\n1: Przegladanie " +
+                    "\n2: Nauka " + "" +
+                    "\n3: Utrwalanie " +
+                    "\n4: Zakończ ");
+            System.out.print("Wybierz tryb: ");
+            mL.selectMode(scanner.nextInt());
+        }
 
-        // tu wykozystamy ModeLauncher do uruchomienia trybow
 
     }
 }
--- src/main/java/emememsy/ModeLauncher.java
+++ src/main/java/emememsy/ModeLauncher.java
@@ -1,41 +1,64 @@
 package emememsy;
 
 
-import emememsy.modes.BrowserMode;
-import emememsy.modes.LearnMode;
-import emememsy.modes.Mode;
-import emememsy.modes.RepeatMode;
+import emememsy.modes.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 //klasa tworzaca oiekt wybranego trybu - "uruchomienie" (...?)
 public class ModeLauncher {
 
+    private int modeInt;
+    private final BrowserMode browserMode = new BrowserMode();
+    private final LearnMode learnMode = new LearnMode();
+    private final RepeatMode repeatMode = new RepeatMode();
+
     public ModeLauncher() {
 
-        BrowserMode browserMode = new BrowserMode();
-        LearnMode learnMode = new LearnMode();
-        RepeatMode repeatMode = new RepeatMode();
 
+//        Map<Integer, Mode> modeMap = new HashMap<Integer, Mode>();
+//        modeMap.put(1, browserMode);
+//        modeMap.put(2, learnMode);
+//        modeMap.put(3, repeatMode);
+//
+//        for (Map.Entry<String, Mode> entry : modeMap.entrySet()) {
+//            //akcja dla iteracji ww mapy
 
-        Map<String, Mode> modeMap = new HashMap<String, Mode>();
-        modeMap.put("b", browserMode);
-        modeMap.put("l", learnMode);
-        modeMap.put("r", repeatMode);
+//        entry.getValue();
+    }
 
-        for (Map.Entry<String, Mode> entry : modeMap.entrySet()) {
-            //akcja dla iteracji ww mapy
+    public void selectMode(int modeInt) {
+
+        switch (modeInt) {
+            case 1:
+                System.out.println("Witamy w trybie: " + browserMode.getModeName());
+                System.out.println("Opis trybu: " + browserMode.getModeDescription());
+                browserMode.launchMode();
+                break;
+            case 2:
+                System.out.println("Witamy w trybie: " + learnMode.getModeName());
+                System.out.println("Opisb trybu: " + learnMode.getModeDescription());
+                break;
+            case 3:
+                System.out.println("Witamy w trybie: " + repeatMode.getModeName());
+                System.out.println("Opis trybu : " + repeatMode.getModeDescription());
+                break;
+            case 4:
+                System.exit(0);
+            default:
+                System.out.println("Sprobuj jeszcze raz!");
         }
 
+    }
 
 
+    public int getModeInt() {
+        return modeInt;
+    }
 
-    //konstruktor tworzacy obiekt wybranego trybu
-    //public ModeLauncher( argument-okreslajacy-tryb ) {
+    public void setModeInt(int modeInt) {
+        this.modeInt = modeInt;
     }
 }
 
