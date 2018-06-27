d0c629259a1e0338665d7441bf5e2f5f4790f16d
Marcin Piatek <marcinpt@onet.pl>
2018-06-27 16:18:25 +0200

    added modes

---
+++ src/main/java/com/infoshareacademy/emememsy/modes/BrowserMode.java
@@ -0,0 +1,46 @@
+package com.infoshareacademy.emememsy.modes;
+
+
+import com.infoshareacademy.emememsy.App;
+
+import java.util.Scanner;
+
+public class BrowserMode extends Mode {
+
+    private static Scanner scanner = new Scanner(System.in); //skaner do wprowadzenia poz menu
+
+    //konstruktor objektu trybu "ustawiający" jego nazwe i opis ktore z automatu sa wypisywane przez .lounchMode (rodzica)
+    public BrowserMode(String modeName, String modeDescriptopn) {
+        super(modeName, modeDescriptopn);
+    }
+
+    //realizacja menu dla objektu tego trybu (tej klasy)
+    public void selectMenu(Integer menuInt) {
+
+        switch (menuInt) {
+            case 1:
+                //TODO replace sout by Monika's function
+                System.out.println("\n\n\nTu wywolanie funkcji wwywolania pary slowo-tlumaczenie \n\n\n");
+                launchMode();
+                break;
+            case 2:
+                App.mainMenu();
+                break;
+            default:
+                System.out.println("Sprobuj jeszcze raz: ");
+        }
+    }
+
+    // "uruchomienie" trybu - ekran powitalny plus pobranie nr menu od usera
+    @Override
+    public void launchMode() {
+        super.launchMode(); //wywolanie ciala metody klasy-rodzica - wspolnej dla wszystkich obiektow od typu Mode
+
+        // ponizej specyficzne display dla kazdego z trybow tutaj takie:
+        System.out.println("\nMasz do wyboru:\n1: Wyswietl slowo-tłumaczenie\n2: Wyjście do Menu Głównego\nWybierz: ");
+        selectMenu(scanner.nextInt()); //wprowadzenie usera jako arg bezp do metody selectMenu()
+    }
+}
+
+
+
---
+++ src/main/java/com/infoshareacademy/emememsy/modes/Mode.java
@@ -0,0 +1,21 @@
+package com.infoshareacademy.emememsy.modes;
+
+public abstract class Mode {
+
+    private String modeName = "";
+    private String modeDescriptopn = "";
+
+    public Mode(String modeName, String modeDescriptopn) {
+        this.modeName = modeName;
+        this.modeDescriptopn = modeDescriptopn;
+    }
+
+    protected Mode() {
+    }
+
+    public void launchMode() {
+        System.out.println("\nJestes w trybie: " + this.modeName);
+        System.out.println("Opis: " + this.modeDescriptopn);
+
+    }
+}
