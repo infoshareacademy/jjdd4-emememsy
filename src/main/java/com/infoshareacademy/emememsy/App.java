01c4dc286d79b34d451828a3f604f84f67257256
Marcin Piatek <marcinpt@onet.pl>
2018-06-27 16:13:26 +0200

    merged with develop branch. Resolved every conflict with it.

--- src/main/java/com/infoshareacademy/emememsy/App.java
+++ src/main/java/com/infoshareacademy/emememsy/App.java
@@ -6,6 +6,7 @@ import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -13,6 +14,22 @@ public class App {
     }
 
     private void run() {
+        mainMenu();
+    }
 
+    // funkcja czyszczaca ekran
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // start - główne menu prg
+    public static void mainMenu() {
+        ModeLauncher modeLauncher = new ModeLauncher();
+        Scanner scanner = new Scanner(System.in);
+        clearScreen();
+        System.out.println("Witamy w aplikacji do nauki słówek - myWords.");
+        System.out.println("Masz do wyboru:\n1: Przegladanie\n2: Nauka\n3: Utrwalanie\n4: Zakończ\nWybierz tryb:");
+        modeLauncher.selectMode(scanner.nextInt());
     }
 }
