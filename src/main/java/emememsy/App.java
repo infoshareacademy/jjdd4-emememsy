7f983102830b5e6e39ff6a12ace7f084e3fca35e
Marcin Piatek <marcinpt@onet.pl>
2018-06-27 15:53:06 +0200

    removed lines from App

--- src/main/java/emememsy/App.java
+++ src/main/java/emememsy/App.java
@@ -7,25 +7,6 @@ import java.util.Scanner;
  */
 
 public class App {
-    private static ModeLauncher modeLauncher = new ModeLauncher();
-    private static Scanner scanner = new Scanner(System.in);
 
-    // funkcja czyszczaca ekran
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
-    // start - główne menu prg
-    public static void mainMenu() {
-        clearScreen();
-        System.out.println("Witamy w aplikacji do nauki słówek - myWords.");
-        System.out.println("Masz do wyboru:\n1: Przegladanie\n2: Nauka\n3: Utrwalanie\n4: Zakończ\nWybierz tryb:");
-        modeLauncher.selectMode(scanner.nextInt());
-    }
-
-    public static void main(String[] args) {
-        mainMenu();
-    }
 }
 
