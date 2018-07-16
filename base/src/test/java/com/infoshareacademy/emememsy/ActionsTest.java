da2de6f205b362877e3e7fe8104cd00c8525f97d
Marcin Piatek <marcinpt@onet.pl>
2018-07-16 16:35:40 +0200

    created 2nd test class Action

--- base/src/test/java/com/infoshareacademy/emememsy/ActionsTest.java
+++ base/src/test/java/com/infoshareacademy/emememsy/ActionsTest.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.emememsy;
 
+import com.infoshareacademy.emememsy.model.SingleWord;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,15 +11,15 @@ import static org.junit.Assert.*;
 
 public class ActionsTest {
 
+    //testing if SingleWord obj is for BrowserMode (counter == 0)
     @Test
     public void pickRandomBrowserMode() {
         //GIVEN
         List<SingleWord> myBMWordList = new ArrayList<>();
 
-
         myBMWordList.add(new SingleWord("dom", "chair", "dom", 0));
-        myBMWordList.add(new SingleWord("ziemniak", "JEDZENIE", "something", 23));
-        myBMWordList.add(new SingleWord("test", "test", "test-category", 100));
+        myBMWordList.add(new SingleWord("ziemniak", "JEDZENIE", "something", 0));
+        myBMWordList.add(new SingleWord("test", "test", "test-category", 0));
 
         //WHEN
         Actions actions = new Actions();
@@ -28,15 +29,23 @@ public class ActionsTest {
         assertTrue(myBMWordList.contains(result));
     }
 
+    //testing if SingleWord obj is for LearnMode (counter is between 1 and 4)
     @Test
     public void pickRandomLearnMode() {
         //GIVEN
         List<SingleWord> myLMWordList = new ArrayList<>();
-        myLMWordList.add(new SingleWord())
+
+        myLMWordList.add(new SingleWord("test1", "test1", "test1", 1));
+        myLMWordList.add(new SingleWord("test2", "test2", "test2", 2));
+        myLMWordList.add(new SingleWord("test3", "test3", "test3", 3));
+        myLMWordList.add(new SingleWord("test4", "test4", "test4", 4));
 
         //WHEN
+        Actions actions = new Actions();
+        SingleWord result = actions.pickRandomLearnMode(myLMWordList);
 
         //THEN
+        assertTrue(myLMWordList.contains(result));
     }
 
     @Test
