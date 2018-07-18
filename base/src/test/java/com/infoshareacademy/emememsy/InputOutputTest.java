fca54aad011e08a35d5b48d3d11b26954d05b3ed
marcinpt <marcinpt@onet.pl>
2018-07-18 07:38:32 +0200

    InputOutputTest in progress

---
+++ base/src/test/java/com/infoshareacademy/emememsy/InputOutputTest.java
@@ -0,0 +1,22 @@
+package com.infoshareacademy.emememsy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputOutputTest {
+
+    @Test
+    public void createListOfWordsOmmitProperties() {
+        //GIVEN
+
+
+        //WHEN
+
+        //THEN
+    }
+
+    @Test
+    public void createListOfWords() {
+    }
+}
