9f2c2e36e28297104b85412a102e6cdf3d50156b
Marcin Piatek <marcinpt@onet.pl>
2018-07-19 16:57:59 +0200

    InputOutputTest in progress

--- base/src/test/java/com/infoshareacademy/emememsy/InputOutputTest.java
+++ base/src/test/java/com/infoshareacademy/emememsy/InputOutputTest.java
@@ -1,13 +1,45 @@
 package com.infoshareacademy.emememsy;
 
+import com.opencsv.CSVReader;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class InputOutputTest {
 
     @Test
     public void createListOfWords() {
+        //GIVEN
+        boolean isUppercase = true;
+        try {
+
+            String CSVfilePath = new File("").getAbsolutePath() + "/src/test/input-words-test.csv";
+            CSVReader reader = new CSVReader(new FileReader(CSVfilePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List<SingleWord> singleWordList = new ArrayList<>();
+        singleWordList.add(new SingleWord("WORD1", "TRANS1","CAT1",0));
+        singleWordList.add(new SingleWord("WORD2", "TRANS2","CAT2",0));
+
+//        when(properties.get()).thenReturn("")
+
+        //WHEN
+        List<SingleWord> result = InputOutput.createListOfWords();
+
+
+        //THEN
+        assertTrue(singleWordList.equals(result));
+
     }
 
     @Test
--- base/src/test/java/com/infoshareacademy/emememsy/PropertiesReaderTest.java
+++ base/src/test/java/com/infoshareacademy/emememsy/PropertiesReaderTest.java
@@ -18,7 +18,8 @@ public class PropertiesReaderTest {
         Map<String, String> testProperties = new HashMap<>();
         testProperties.put("testKey1", "testValue1");
         testProperties.put("testKey2", "testValue2");
-        String filePath = new File("").getAbsolutePath() + "/src/test/config.properties";
+
+        String filePath = new File("").getAbsolutePath() + "/src/test/config-test1.properties";
 
         //WHEN
         Map<String, String> result = propertiesReader.read(filePath);
