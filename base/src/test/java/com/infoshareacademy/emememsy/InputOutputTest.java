0993dcda1a90002df07ec4e5190331d2f13d3e91
marcinpt <marcinpt@onet.pl>
2018-07-19 07:37:09 +0200

    PropertiesReaderTest class - done

---
+++ base/src/test/java/com/infoshareacademy/emememsy/InputOutputTest.java
@@ -0,0 +1,16 @@
+package com.infoshareacademy.emememsy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputOutputTest {
+
+    @Test
+    public void createListOfWords() {
+    }
+
+    @Test
+    public void writeToCSV() {
+    }
+}
