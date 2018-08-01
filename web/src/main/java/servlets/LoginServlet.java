c57d87251cb380372193d238852b45e78a5a7d95
Monika Luba <lubamonika@gmail.com>
2018-08-01 16:52:34 +0200

    added loggers

--- web/src/main/java/servlets/LoginServlet.java
+++ web/src/main/java/servlets/LoginServlet.java
@@ -1,24 +1,22 @@
 package servlets;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import freemarker.TemplateProvider;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 
-import javax.inject.Inject;
-import javax.servlet.ServletException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LearnModeServlet.class);
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
@@ -27,6 +25,8 @@ public class LoginServlet extends HttpServlet {
 
         try {
             String idToken = req.getParameter("id_token");
+            LOG.info(req.getParameterMap().toString());
+            LOG.info(idToken);
             GoogleIdToken.Payload payLoad = IdTokenVerifierAndParser.getPayload(idToken);
             String name = (String) payLoad.get("name");
             String email = payLoad.getEmail();
