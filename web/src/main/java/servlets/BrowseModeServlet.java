1e32f393420c1dc43808984f32b7763f943771ac
Sebastian Bieluga <sbiely22@gmail.com>
2018-08-01 16:49:56 +0200

    sroda

--- web/src/main/java/servlets/BrowseModeServlet.java
+++ web/src/main/java/servlets/BrowseModeServlet.java
@@ -31,57 +31,58 @@ public class BrowseModeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    throw new RuntimeException();
 
-        String category = req.getParameter("category");
-        String mode = req.getParameter("mode");
-
-        if (category == null || category.isEmpty()) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            LOG.error("Problem with loading the correct module");
-            return;
-        }
-
-        Template template = templateProvider.getTemplate(getServletContext(), "browse-mode.ftlh");
-
-        SingleWord singleWord = selectWord(req, resp);
-
-        Map<String, Object> model = new HashMap<>();
-        model.put("category", category);
-        model.put("singleWord", singleWord);
-        model.put("mode", mode);
-
-        resp.setContentType("text/html;charset=UTF-8");
-        LOG.info("The file was load corectly");
-        try {
-            template.process(model, resp.getWriter());
-        } catch (TemplateException e) {
-            e.printStackTrace();
-            LOG.error("Problems with template", e.getMessage());
-        }
-    }
-
-    private SingleWord selectWord (HttpServletRequest req, HttpServletResponse resp){
-
-        List<SingleWord> listOfWords = new ArrayList<>();
-
-        if (req.getParameter("category").equalsIgnoreCase("wszystkie")) {
-            listOfWords =  singleWordDao.findByAllCategoriesBrowseMode();
-        } else {
-            listOfWords = singleWordDao.findByCategoryBrowseMode(req.getParameter("category"));
-        }
-
-        if(listOfWords.isEmpty()){
-            return null;
-        } else {
-            Random randomGenerator = new Random();
-            int random = randomGenerator.nextInt(listOfWords.size());
-            SingleWord singleWord = listOfWords.get(random);
-            singleWord.setCounter(singleWord.getCounter() + 1);
-            singleWord.setDisplayed(singleWord.getDisplayed() + 1);
-            singleWordDao.update(singleWord);
-            return singleWord;
-        }
-    }
+//          String category = req.getParameter("category");
+//          String mode = req.getParameter("mode");
+//
+//        if (category == null || category.isEmpty()) {
+//            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            LOG.error("Problem with loading the correct module");
+//            return;
+//        }
+//
+//        Template template = templateProvider.getTemplate(getServletContext(), "browse-mode.ftlh");
+//
+//        SingleWord singleWord = selectWord(req, resp);
+//
+//        Map<String, Object> model = new HashMap<>();
+//        model.put("category", category);
+//        model.put("singleWord", singleWord);
+//        model.put("mode", mode);
+//
+//        resp.setContentType("text/html;charset=UTF-8");
+//        LOG.info("The file was load corectly");
+//        try {
+//            template.process(model, resp.getWriter());
+//        } catch (TemplateException e) {
+//            e.printStackTrace();
+//            LOG.error("Problems with template", e.getMessage());
+//        }
+//    }
+//
+//    private SingleWord selectWord (HttpServletRequest req, HttpServletResponse resp){
+//
+//        List<SingleWord> listOfWords = new ArrayList<>();
+//
+//        if (req.getParameter("category").equalsIgnoreCase("wszystkie")) {
+//            listOfWords =  singleWordDao.findByAllCategoriesBrowseMode();
+//        } else {
+//            listOfWords = singleWordDao.findByCategoryBrowseMode(req.getParameter("category"));
+//        }
+//
+//        if(listOfWords.isEmpty()){
+//            return null;
+//        } else {
+//            Random randomGenerator = new Random();
+//            int random = randomGenerator.nextInt(listOfWords.size());
+//            SingleWord singleWord = listOfWords.get(random);
+//            singleWord.setCounter(singleWord.getCounter() + 1);
+//            singleWord.setDisplayed(singleWord.getDisplayed() + 1);
+//            singleWordDao.update(singleWord);
+//            return singleWord;
+//        }
+   }
 }
 
 
