86183bd608bbf9b78fbaa1114408f68316d686c6
Monika Luba <lubamonika@gmail.com>
2018-08-02 13:04:06 +0200

    added one more logger to identify issue with oauth

--- web/src/main/java/servlets/IdTokenVerifierAndParser.java
+++ web/src/main/java/servlets/IdTokenVerifierAndParser.java
@@ -4,10 +4,13 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class IdTokenVerifierAndParser {
     private static final String GOOGLE_CLIENT_ID = "908551710285-0p3j7v2j1okoor79871acul99t3u00gj.apps.googleusercontent.com";
+    private static final Logger LOG = LoggerFactory.getLogger(IdTokenVerifierAndParser.class);
 
     public static GoogleIdToken.Payload getPayload (String tokenString) throws Exception {
 
@@ -16,6 +19,7 @@ public class IdTokenVerifierAndParser {
                 new GoogleIdTokenVerifier(new NetHttpTransport(), jacksonFactory);
 
         GoogleIdToken token = GoogleIdToken.parse(jacksonFactory, tokenString);
+        LOG.info("Token to verify: " + token);
 
         if (googleIdTokenVerifier.verify(token)) {
             GoogleIdToken.Payload payload = token.getPayload();
