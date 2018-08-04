a1e4f62fef2d6e3860cee778d9c6790bf1737222
marcinpt <marcinpt@onet.pl>
2018-08-04 23:52:08 +0200

    sending test mail - one of many front versions (templates)

--- web/src/main/java/servlets/MailDispatcherServlet.java
+++ web/src/main/java/servlets/MailDispatcherServlet.java
@@ -1,42 +1,63 @@
 package servlets;
 
 import data.MailSenderBean;
+import freemarker.TemplateProvider;
+import freemarker.template.Template;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/mail-dispatcher")
 public class MailDispatcherServlet extends HttpServlet {
 
 
+
+
     @EJB
     private MailSenderBean mailSender;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession(true);
+        Boolean isAuthorised = (Boolean)session.getAttribute("userName");
+        if(isAuthorised == null|| isAuthorised == false) {
+            resp.sendRedirect("/index.jsp");
+        }
+    }
+
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 
         try {
-            response.setContentType("text/html; charset=UTF-8");
+            resp.setContentType("text/html; charset=UTF-8");
 
-            String toEmail = request.getParameter("email");
+            String toEmail = req.getParameter("email");
+            String subject = "Raport z aplikacji myWords";
+            String message = "To jest raport wygenerowany z aplikacji myWord...";
+
 
             //data for connection - intentional hardcoding solution just for testing
             String fromEmail = "emememsy2018@gmail.com";
             String username = "emememsy2018";
             String password = "summer2018";
 
-            //call to mail sender bean here
+            //call to mail sender bean inside this block-----
 
-            mailSender.sendEmail(fromEmail, username, password, toEmail, );
+            mailSender.sendEmail(fromEmail, username, password, toEmail, subject, message);
 
+            //-----------------------------------------------
 
-            //---------------------
-
-            response.sendRedirect("/mail-status");
+            resp.sendRedirect("/mail-status");
 
 
 
@@ -47,9 +68,6 @@ public class MailDispatcherServlet extends HttpServlet {
 
     }
 
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//    }
 
 
 }
